38e3a8fce7f0eb8e4518919d67633db129d55556
ElongMusk <71707464+ElongMusk@users.noreply.github.com>
2023-01-16 18:24:25 +0000

    created a draw function

--- main.py
+++ main.py
@@ -9,24 +9,27 @@ TRACK = scale_image(pygame.image.load("imgs/track.png"), 0.9)
 TRACK_BOARDER = scale_image(pygame.image.load("imgs/track-border.png"), 0.9)
 FINISH = pygame.image.load("imgs/finish.png")
 
-RED_CAR = scale_image(pygame.image.load("imgs/red-car.png"), 0.55
-PURPLE_CAR = pygame.image.load("imgs/purple-car.png")
+RED_CAR = scale_image(pygame.image.load("imgs/red-car.png"), 0.55)
+PURPLE_CAR = scale_image(pygame.image.load("imgs/purple-car.png"), 0.55)
 
 WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Racing game!!")
 
+def draw(win, images):
+    for img, pos in images:
+        win.blit(img, pos)
+    
+
 FPS = 60
 run = True
 clock = pygame.time.Clock()
+images = [(GRASS, (0,0)), (TRACK,(0,0))]
 
 while run:
     clock.tick(FPS)
     
-    WIN.blit(GRASS,(0,0))
-    WIN.blit(TRACK,(0,0))
-    WIN.blit(FINISH,(0,0))
-    
+    draw(WIN, images)
     pygame.display.update()
     
     
