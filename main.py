45bd2d3254bb4c1d8249dd08d871b6611b24e446
ElongMusk <71707464+ElongMusk@users.noreply.github.com>
2023-01-16 19:20:27 +0000

    Rotation functionallity has beed added to the car.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 import math
 import pygame
 from utils import scale_image
+from utils import blit_rotate_center
 
 GRASS = scale_image(pygame.image.load("imgs/grass.jpg"), 2.5)
 TRACK = scale_image(pygame.image.load("imgs/track.png"), 0.9)
@@ -16,22 +17,50 @@ WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Racing game!!")
 
-def draw(win, images):
+
+class AbstractCar:
+    IMG = RED_CAR
+    
+    def __init__(self, max_vel, rotation_vel):
+        self.max_vel = max_vel
+        self.vel = 0
+        self.rotation_vel = rotation_vel
+        self.angle = 0
+        self.img = self.IMG
+        self.x, self.y = self.START_POS
+        
+    def rotate(self, left=False, right=True):
+        if left:
+            self.angle += self.rotation_vel
+        elif right:
+            self.angle -= self.rotation_vel
+            
+    def draw(self, win):
+        blit_rotate_center(win, self.img, (self.x, self.y), self.angle)
+        
+class PlayerCar(AbstractCar):
+    IMG = RED_CAR
+    START_POS = (180,200)
+    
+
+
+
+def draw(win, images, player_car):
     for img, pos in images:
         win.blit(img, pos)
-    
+    player_car.draw(win)
+    pygame.display.update()
 
 FPS = 60
 run = True
 clock = pygame.time.Clock()
 images = [(GRASS, (0,0)), (TRACK,(0,0))]
+player_car = PlayerCar(4,4)
 
 while run:
     clock.tick(FPS)
     
-    draw(WIN, images)
-    pygame.display.update()
-    
+    draw(WIN, images, player_car)
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
