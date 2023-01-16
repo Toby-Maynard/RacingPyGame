4833cecae4e5804a50b4568cd74e820b9254b856
ElongMusk <71707464+ElongMusk@users.noreply.github.com>
2023-01-16 17:46:32 +0000

    Displaying images and resizing them using the new utils.py

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import time
 import math
 import pygame
+from utils import scale_image
 
+GRASS = scale_image(pygame.image.load("imgs/grass.jpg"), 2.5)
+TRACK = scale_image(pygame.image.load("imgs/track.png"), 0.9)
 
-GRASS = pygame.image.load("imgs/grass.jpg")
-TRACK = pygame.image.load("imgs/track.png")
-
-TRACK_BOARDER = pygame.image.load("imgs/track-border.png")
+TRACK_BOARDER = scale_image(pygame.image.load("imgs/track-border.png"), 0.9)
 FINISH = pygame.image.load("imgs/finish.png")
 
-RED_CAR = pygame.image.load("imgs/red-car.png")
+RED_CAR = scale_image(pygame.image.load("imgs/red-car.png"), 0.55
 PURPLE_CAR = pygame.image.load("imgs/purple-car.png")
 
 WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Racing game!!")
 
+FPS = 60
 run = True
+clock = pygame.time.Clock()
+
 while run:
+    clock.tick(FPS)
+    
+    WIN.blit(GRASS,(0,0))
+    WIN.blit(TRACK,(0,0))
+    WIN.blit(FINISH,(0,0))
+    
+    pygame.display.update()
+    
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
